1ae40b572c950c04484aeaff2c8ba53cc3f2d41f
aravind54 <aravindcranjan54@gmail.com>
2019-07-26 17:41:54 +0530

    install cors to provide cross origin request compatibility-install nodemon

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-const argv = require('yargs')
-  .usage('Usage: $0 <command> [options]')
-  .help('h')
-  .alias('h', 'help')
-  .describe('port', 'port number for the server')
-  .describe('schema_file_path', 'Path to the graphql schema')
-  .example("node $0 --schema_file_path '../awb-fake2/schema.graphql' --port 3006")
-  .argv;
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools');
-const { importSchema } = require('graphql-import')
-const schema = importSchema(argv.schema_file_path)
-const MockList = require('graphql-tools').MockList;
+const argv = require("yargs")
+  .usage("Usage: $0 <command> [options]")
+  .help("h")
+  .alias("h", "help")
+  .describe("port", "port number for the server")
+  .describe("schema_file_path", "Path to the graphql schema")
+  .example(
+    "node $0 --schema_file_path '../awb-fake2/schema.graphql' --port 3006"
+  ).argv;
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const {
+  makeExecutableSchema,
+  addMockFunctionsToSchema
+} = require("graphql-tools");
+const { importSchema } = require("graphql-import");
+const schema = importSchema(argv.schema_file_path);
+const MockList = require("graphql-tools").MockList;
 
 // console.log(util.inspect(schema))
 const resolvers = {
-  Url: () => 'Fix',
+  Url: () => "Fix",
   DateTime: () => {
     return new Date();
   },
   Query: () => ({
-    relationshipManagers: () => new MockList(1),
+    relationshipManagers: () => new MockList(1)
   }),
   RelationshipManager: () => ({
     reportingRelationshipManagers: () => new MockList(5),
     aums: () => new MockList(5)
-  }),
+  })
 };
-preserveResolvers = true
+preserveResolvers = true;
 
 const schemaServer = makeExecutableSchema({ typeDefs: schema });
 
@@ -43,10 +48,15 @@ addMockFunctionsToSchema({
 const app = express();
 
 // The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schemaServer }));
+app.use(
+  "/graphql",
+  cors(),
+  bodyParser.json(),
+  graphqlExpress({ schema: schemaServer })
+);
 
 // GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 // Start the server
 app.listen(argv.port || 3000, "0.0.0.0", () => {
